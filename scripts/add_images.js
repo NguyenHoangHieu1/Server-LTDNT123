ac81b33f6a845e2792619fed22aa14b3a681af78
hoanghieu <tramyfro@gmail.com>
2025-05-08 12:20:51 +0700

    update

--- scripts/add_images.js
+++ scripts/add_images.js
@@ -15,41 +15,41 @@ mongoose.connect("mongodb://localhost:27017/pcpartpicker", {
 });
 
 const cpuImages = [
-  "../uploads/cpu/cpu1.jpg",
-  "../uploads/cpu/cpu2.jpg",
-  "../uploads/cpu/cpu3.jpg",
+  "/uploads/cpu/cpu1.jpg",
+  "/uploads/cpu/cpu2.jpg",
+  "/uploads/cpu/cpu3.jpg",
 ];
 
 const driveImages = [
-  "../uploads/drive/drive1.jpg",
-  "../uploads/drive/drive2.jpg",
-  "../uploads/drive/drive3.jpg",
+  "/uploads/drive/drive1.jpg",
+  "/uploads/drive/drive2.jpg",
+  "/uploads/drive/drive3.jpg",
 ];
 
 const gpuImages = [
-  "../uploads/gpu/gpu1.jpg",
-  "../uploads/gpu/gpu2.jpg",
-  "../uploads/gpu/gpu3.jpg",
+  "/uploads/gpu/gpu1.jpg",
+  "/uploads/gpu/gpu2.jpg",
+  "/uploads/gpu/gpu3.jpg",
 ];
 const keyboardImages = [
-  "../uploads/keyboard/keyboard1.jpg",
-  "../uploads/keyboard/keyboard2.jpg",
-  "../uploads/keyboard/keyboard3.jpg",
+  "/uploads/keyboard/keyboard1.jpg",
+  "/uploads/keyboard/keyboard2.jpg",
+  "/uploads/keyboard/keyboard3.jpg",
 ];
 const motherboardImages = [
-  "../uploads/motherboard/motherboard1.jpg",
-  "../uploads/motherboard/motherboard2.jpg",
-  "../uploads/motherboard/motherboard3.jpg",
+  "/uploads/motherboard/motherboard1.jpg",
+  "/uploads/motherboard/motherboard2.jpg",
+  "/uploads/motherboard/motherboard3.jpg",
 ];
 const mouseImages = [
-  "../uploads/mouse/mouse1.jpg",
-  "../uploads/mouse/mouse2.jpg",
-  "../uploads/mouse/mouse3.jpg",
+  "/uploads/mouse/mouse1.jpg",
+  "/uploads/mouse/mouse2.jpg",
+  "/uploads/mouse/mouse3.jpg",
 ];
 const ramImages = [
-  "../uploads/ram/ram1.jpg",
-  "../uploads/ram/ram2.jpg",
-  "../uploads/ram/ram3.jpg",
+  "/uploads/ram/ram1.jpg",
+  "/uploads/ram/ram2.jpg",
+  "/uploads/ram/ram3.jpg",
 ];
 
 async function updateImages() {
